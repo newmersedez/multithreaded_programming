b229e29137bf85af9fa0f0a921bfee2c7f2a1f67
newmersedez <trishkk2001@gmail.com>
2021-10-22 21:10:30 +0300

    finally finished minishell

--- Lesson 5/pipes/minishell.cpp
+++ Lesson 5/pipes/minishell.cpp
@@ -11,7 +11,7 @@
 
 #define COMMAND_BUFSIZ	16
 
-void split(std::vector<std::string>& commands_vec,
+static void split(std::vector<std::string>& commands_vec,
 			const std::string& command, std::string delim)
 {
 	size_t 				i = 0, length = command.length();
@@ -31,65 +31,47 @@ void split(std::vector<std::string>& commands_vec,
 	}
 }
 
-void exec_pipe_command(std::vector<std::string> commands_vec)
+static void exec_pipe_command(std::vector<std::string> commands_vec)
 {
-	int		fd[2], status;
-	pid_t	cpid;
+	int		fd[2], fdd = 0, status;
+	pid_t	child_pid;
 
 	for (const auto& command: commands_vec)
 	{
-		std::vector<std::string>	exec_vec;
-		size_t						size = 0;
-		char						*exec_array[COMMAND_BUFSIZ];
+        std::vector<std::string>    exec_vec;
+		size_t                      size = 0;
+		char                        *exec_array[COMMAND_BUFSIZ];
 
 		split(exec_vec, command, " ");
 		for (size_t i = 0; i < exec_vec.size(); i++)
 			exec_array[size++] = const_cast<char *>(exec_vec[i].c_str());
 		exec_array[size] = NULL;
-		
-		cpid = fork();
-		if (cpid == 0)
+		if (pipe(fd) < 0)
+			{ perror("pipe"), exit(EXIT_FAILURE); }
+		child_pid = fork();
+		if (child_pid < 0)
+			{ perror("fork"), exit(EXIT_FAILURE); }
+		else if (child_pid == 0)
 		{
-			// left command
+			if (dup2(fdd, 0) < 0)
+				{ perror("dup2"), exit(EXIT_FAILURE); }
 			if (command != commands_vec.back())
 			{
-				// close(fd[0]);
-				dup2(fd[1], STDOUT_FILENO);
-				execvp(exec_array[0], exec_array);
+				if (dup2(fd[1], 1) < 0)
+					{ perror("dup2"), exit(EXIT_FAILURE); }
 			}
-			// right command
-			if (command != commands_vec.front())
-			{
-				// close(fd[1]);
-				dup2(fd[0], STDIN_FILENO);
-				execvp(exec_array[0], exec_array);
-			}
+			close(fd[0]);
+			execvp(exec_array[0], exec_array);
+			perror("execvp");
+			exit(EXIT_SUCCESS);
 		}
-		close(fd[0]);
-		close(fd[1]);
-		waitpid(-1, &status, 1);
+		else
+		{
+			waitpid(child_pid, &status, 0);
+			close(fd[1]);
+			fdd = fd[0];
+		}
 	}
-
-	// // left pipe command
-	// cpid = fork();
-	// if (cpid == 0)
-	// {
-	// 	close(fd[0]);
-	// 	dup2(fd[1], STDOUT_FILENO);
-	// 	execlp("/bin/sh", "sh", "-c", commands_vec[0].c_str(), NULL);
-	// }
-	// // right pipe command
-	// cpid = fork();
-	// if (cpid == 0)
-	// {
-	// 	close(fd[1]);
-	// 	dup2(fd[0], STDIN_FILENO);
-	// 	execlp("/bin/sh", "sh", "-c", commands_vec[1].c_str(), NULL);
-	// }
-	// close(fd[0]);
-	// close(fd[1]);
-	// waitpid(-1, &status, 0);
-	// waitpid(-1, &status, 0);
 }
 
 int main()
