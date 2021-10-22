c2aacc70b2fb25f7a6a092a34bffbc9c390e9668
newmersedez <trishkk2001@gmail.com>
2021-10-22 19:50:48 +0300

    =)

--- Lesson 5/pipes/minishell.cpp
+++ Lesson 5/pipes/minishell.cpp
@@ -1,3 +1,4 @@
+// execlp("/bin/sh", "sh", "-c", command.c_str(), NULL);
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -35,26 +36,60 @@ void exec_pipe_command(std::vector<std::string> commands_vec)
 	int		fd[2], status;
 	pid_t	cpid;
 
-	//left pipe command
-	cpid = fork();
-	if (cpid == 0)
+	for (const auto& command: commands_vec)
 	{
+		std::vector<std::string>	exec_vec;
+		size_t						size = 0;
+		char						*exec_array[COMMAND_BUFSIZ];
+
+		split(exec_vec, command, " ");
+		for (size_t i = 0; i < exec_vec.size(); i++)
+			exec_array[size++] = const_cast<char *>(exec_vec[i].c_str());
+		exec_array[size] = NULL;
+		
+		cpid = fork();
+		if (cpid == 0)
+		{
+			// left command
+			if (command != commands_vec.back())
+			{
+				// close(fd[0]);
+				dup2(fd[1], STDOUT_FILENO);
+				execvp(exec_array[0], exec_array);
+			}
+			// right command
+			if (command != commands_vec.front())
+			{
+				// close(fd[1]);
+				dup2(fd[0], STDIN_FILENO);
+				execvp(exec_array[0], exec_array);
+			}
+		}
 		close(fd[0]);
-		dup2(fd[1], STDOUT_FILENO);
-		execlp("/bin/sh", "sh", "-c", commands_vec[0].c_str(), NULL);
-	}
-	//right pipe command
-	cpid = fork();
-	if (cpid == 0)
-	{
 		close(fd[1]);
-		dup2(fd[0], STDIN_FILENO);
-		execlp("/bin/sh", "sh", "-c", commands_vec[1].c_str(), NULL);
+		waitpid(-1, &status, 1);
 	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(-1, &status, 0);
-	waitpid(-1, &status, 0);
+
+	// // left pipe command
+	// cpid = fork();
+	// if (cpid == 0)
+	// {
+	// 	close(fd[0]);
+	// 	dup2(fd[1], STDOUT_FILENO);
+	// 	execlp("/bin/sh", "sh", "-c", commands_vec[0].c_str(), NULL);
+	// }
+	// // right pipe command
+	// cpid = fork();
+	// if (cpid == 0)
+	// {
+	// 	close(fd[1]);
+	// 	dup2(fd[0], STDIN_FILENO);
+	// 	execlp("/bin/sh", "sh", "-c", commands_vec[1].c_str(), NULL);
+	// }
+	// close(fd[0]);
+	// close(fd[1]);
+	// waitpid(-1, &status, 0);
+	// waitpid(-1, &status, 0);
 }
 
 int main()
